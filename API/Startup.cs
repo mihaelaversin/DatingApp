96333837b9a8b9b9d3a38a93eb083db3c06c0b6f
Mihaela Versin <mihaelaversin@gmail.com>
2022-01-27 15:21:41 +0200

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,12 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API.Controllers.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -31,15 +37,16 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddApplicationServices (_config);    
             services.AddControllers();
-            services.AddDbContext<DataContext> (options => {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
             services.AddCors();
+            services.AddIdentityServices(_config);
+        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +65,7 @@ namespace API
 
             app.UseCors(x =>  x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
